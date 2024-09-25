cef935c9d6c38c8b6cf60b576ede8bdcb086832a
Opilane TTHK <opilane@tthk.ee>
2024-09-25 13:29:10 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require('express')()
+const port = 8080
+
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./docs/swagger.json');
+
+app.get('/games', (req, res) => {res.send(["Witcher 3","Cyberpunk 2077"])})
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.listen(port, () => {
+    console.log(`API up at: http://localhost:${port}`)
+})
